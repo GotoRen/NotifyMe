26276c0184f70c37529955bf7528db6ee37b468b
GotoRen <r0719en@outlook.jp>
2020-09-10 19:13:00 +0900

    Add sh_test

---
+++ sh_test/cat_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat <<- _HTML_ > cat_test.html
+　 　<html>
+　 　<head>
+			<title>cat_test</title>
+		</head>
+　 　<body>
+　 　 	<h1>hello world</h1>
+　 　</body>
+　 　</html> 
+_HTML_
---
+++ sh_test/cgi_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Content-Type: text/html; charset=utf-8"
+echo ""
+echo "
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ドキュメント</title>
+</head>
+<body>
+<p>ヒアドキュメントで、<br>
+テキスト出力も自由自在！！</p>
+</body>
+</html>
+"
