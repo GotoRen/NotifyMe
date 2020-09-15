653101fcaa8e9360140f5615f0d8f39e9ea597fb
GotoRen <r0719en@outlook.jp>
2020-09-15 21:40:33 +0900

    Add: work/

---
+++ work/webiopi/static/js/main.js
@@ -0,0 +1,11 @@
+webiopi()
+ 
+function python() {
+    webiopi().callMacro("run_script", 0, callbackGetValue);
+}
+ 
+function callbackGetValue(macro, args, data) {
+    console.log(macro);
+    console.log(args);
+    console.log(data);
+}
